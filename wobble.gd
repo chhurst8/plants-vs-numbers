680a184b2a3b488594098704ef1f23e2ac3714e6
CreggEgg <eggcregg@gmail.com>
2026-07-25 21:33:05 -0400

    wobble

---
+++ wobble.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotation_degrees = sin(Time.get_unix_time_from_system() * 1.5) * 2
+	pass
